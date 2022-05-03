4657c9e1baec1ee84658165789c7cbd3b394e82e
Stephanie Jones <stephanieruthjones@gmail.com>
2022-05-03 16:08:19 -0600

    Delete files

--- fetchProducts.js
+++
@@ -1,9 +0,0 @@
-import fetch from "node-fetch"
-
-const storeUrl = 'https://fakestoreapi.com/products'
-const fetchProducts = async () => {
-    const response = await fetch(storeUrl)
-    const json = await response.json()
-    console.log(json)
-}
-fetchProducts()
